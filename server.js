426172aa87e6526dd9319855d8f0fb131df1ff72
Akuma <sergiiosercopi@gmail.com>
2020-10-17 19:01:26 +0200

    changes to create express server for heroku

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const serveStatic = require("serve-static");
+const history = require("connect-history-api-fallback");
+const enforce = require("express-sslify");
+const app = express();
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+app.use(serveStatic(__dirname + "/dist"));
+app.use(history());
+app.listen(process.env.PORT || 500);
+console.log("running at port 500");
